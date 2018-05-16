3520b7a81223e1250948d600d544a19d3a8ea512
jenniferhsueh <jenhsueh89@hotmail.com>
2018-05-16 21:47:16 +0000

    duplicated dl avatar to delete notes for submission

---
+++ dlAvatarsWithNotes.js
@@ -0,0 +1,56 @@
+var request = require('request');
+var secret = require('./secret');
+var fs = require('fs');
+// console.log(secret); testing correct token
+
+var repoOwner = process.argv[2];
+var repoName = process.argv[3];
+
+console.log('Welcome to the GitHub Avatar Downloader!');
+
+function getRepoContributors(repoOwner, repoName, cb) {
+  if(!repoOwner || !repoName) {
+    throw "FUUUUCK";
+  };
+  var options = {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers: {
+      'User-Agent': 'request',
+      'Authorization' : "token " + secret.GITHUB_TOKEN 
+    }
+  }
+  request.get(options, function (error, response, body) {
+
+  // var url = `https://api.github.com/repos/${repoOwner}/${repoName}/contributors`;
+  // request(url, function (error, response, body) {    //same thing as below
+
+  // request( `https://api.github.com/repos/${repoOwner}/${repoName}/contributors`, function (error, response, body) {
+    
+  response.setEncoding('utf8'); //response is thing i'm getting back, response is only avail when makign a request
+    //logging in callback (below)
+    // console.log('error:', error); // Print the error if one occurred
+    // console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    // console.log('body:', body); // Print the HTML for the Google homepage.
+    cb(error, JSON.parse(body)) //calling the callback
+  });
+
+}       //"lighthouse-labs", "assessment-exam-student" --> to test by hard coding. private repo
+getRepoContributors(repoOwner, repoName, function(err, result) {
+  // console.log("Errors:", err);    //calling the function and this is the callback
+  
+  // console.log("Result:", result);
+  result.forEach(function(item){  //****item is the whole object */
+    console.log(item.avatar_url); //item is the response//
+    var name = item.login
+    downloadImageByURL(item.avatar_url, './avatars/' + name + '.jpg');
+  })
+});
+
+// download image from 'url'
+// store that image in 'filepath'
+function downloadImageByURL(url, filePath) {
+  request(url, function (error, response, body) {
+    // console.log(error);
+    console.log('Downloading image...'); 
+  }).pipe(fs.createWriteStream(filePath));
+}
--- download_avatars.js
+++ download_avatars.js
@@ -1,7 +1,6 @@
 var request = require('request');
 var secret = require('./secret');
 var fs = require('fs');
-// console.log(secret); testing correct token
 
 var repoOwner = process.argv[2];
 var repoName = process.argv[3];
@@ -10,7 +9,7 @@ console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
   if(!repoOwner || !repoName) {
-    throw "FUUUUCK";
+    throw "Please specify two arguments";
   };
   var options = {
     url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
@@ -18,39 +17,25 @@ function getRepoContributors(repoOwner, repoName, cb) {
       'User-Agent': 'request',
       'Authorization' : "token " + secret.GITHUB_TOKEN 
     }
-  }
+  };
   request.get(options, function (error, response, body) {
-
-  // var url = `https://api.github.com/repos/${repoOwner}/${repoName}/contributors`;
-  // request(url, function (error, response, body) {    //same thing as below
-
-  // request( `https://api.github.com/repos/${repoOwner}/${repoName}/contributors`, function (error, response, body) {
     
-  response.setEncoding('utf8'); //response is thing i'm getting back, response is only avail when makign a request
-    //logging in callback (below)
-    // console.log('error:', error); // Print the error if one occurred
-    // console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-    // console.log('body:', body); // Print the HTML for the Google homepage.
-    cb(error, JSON.parse(body)) //calling the callback
+  response.setEncoding('utf8'); 
+    cb(error, JSON.parse(body)) 
   });
+};
 
-}       //"lighthouse-labs", "assessment-exam-student" --> to test by hard coding. private repo
 getRepoContributors(repoOwner, repoName, function(err, result) {
-  // console.log("Errors:", err);    //calling the function and this is the callback
-  
-  // console.log("Result:", result);
-  result.forEach(function(item){  //****item is the whole object */
-    console.log(item.avatar_url); //item is the response//
+  result.forEach(function(item){  
+    console.log(item.avatar_url); 
     var name = item.login
     downloadImageByURL(item.avatar_url, './avatars/' + name + '.jpg');
   })
 });
 
-// download image from 'url'
-// store that image in 'filepath'
+// download image from 'url' & store it in 'filepath'
 function downloadImageByURL(url, filePath) {
   request(url, function (error, response, body) {
-    // console.log(error);
     console.log('Downloading image...'); 
   }).pipe(fs.createWriteStream(filePath));
-}
+};
